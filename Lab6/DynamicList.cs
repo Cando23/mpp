78e1b60f91ccd70907dfadeb521a14c7b5bac8bb
Roman Polishchuk <rmn.polishchuk@gmail.com>
2021-09-15 20:33:10 +0300

    add constructor and Count property

--- Lab6/DynamicList.cs
+++ Lab6/DynamicList.cs
@@ -6,7 +6,17 @@ namespace Lab6
 {
     public class DynamicList<T> : IEnumerable<T>
     {
+        public DynamicList()
+        { 
+            _count = 0;
+            _array = new T[0];
+        }
         private T[] _array;
+        private int _count;
+        public int Count
+        {
+            get { return _count; }
+        }
         public IEnumerator<T> GetEnumerator()
         {
             return ((IEnumerable<T>) _array).GetEnumerator();
