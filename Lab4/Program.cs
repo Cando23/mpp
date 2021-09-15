ce597b2b887ad81113c4f985fbeb689196f2758a
Roman Polishchuk <rmn.polishchuk@gmail.com>
2021-09-15 22:18:05 +0300

    add loading assembly method

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
 
 namespace Lab4
 {
     class Program
     {
+        private static Type[] _types;
+        static void GetTypes(string path)
+        {
+            Assembly assembly = Assembly.LoadFrom(path ?? throw new InvalidOperationException());
+            _types = assembly.GetTypes();
+            _types = _types.OrderBy(type => type.FullName).ToArray();
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Input path to Dll file");
+            string path = Console.ReadLine();
+            try
+            {
+                GetTypes(path);
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
         }
     }
 }
