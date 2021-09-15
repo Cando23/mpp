f26d1c9579d464cbcd3a55f67419e10b2855ab78
Roman Polishchuk <rmn.polishchuk@gmail.com>
2021-09-15 20:31:31 +0300

    add IEnumerable<T> implementation

--- Lab6/DynamicList.cs
+++ Lab6/DynamicList.cs
@@ -1,7 +1,21 @@
-﻿namespace Lab6
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+namespace Lab6
 {
-    public class DynamicList
+    public class DynamicList<T> : IEnumerable<T>
     {
-        
+        private T[] _array;
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IEnumerable<T>) _array).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<T>) _array).GetEnumerator();
+        }
     }
+    
 }
