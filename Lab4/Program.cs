d8eef1832946bc2424048bfcad18f833c5acebfd
Roman Polishchuk <rmn.polishchuk@gmail.com>
2021-09-15 22:50:39 +0300

    add output types method

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -1,26 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
 namespace Lab4
 {
-    class Program
+    static class Program
     {
         private static Type[] _types;
-        static void GetTypes(string path)
+
+        private static void GetTypes(string path)
         {
-            Assembly assembly = Assembly.LoadFrom(path ?? throw new InvalidOperationException());
+            var assembly = Assembly.LoadFrom(path ?? throw new InvalidOperationException());
             _types = assembly.GetTypes();
             _types = _types.OrderBy(type => type.FullName).ToArray();
         }
-        
-        static void Main(string[] args)
+
+        private static void OutputSortedMemberTypes()
+        {
+            foreach (var type in _types)
+            {
+                var members = type.GetMembers();
+                var memberTypes = members.Where(member => 
+                    member.MemberType == MemberTypes.Property ||
+                    member.MemberType == MemberTypes.Field ||
+                    member.MemberType == MemberTypes.NestedType);
+                memberTypes = memberTypes.OrderBy(memberType => memberType.Name).ToArray();
+                Console.WriteLine(type.FullName);
+                foreach (var memberType in memberTypes)
+                {
+                    Console.WriteLine(memberType.Name);
+                }
+            }
+        }
+
+        private static void Main(string[] args)
         {
             Console.WriteLine("Input path to Dll file");
-            string path = Console.ReadLine();
+            var path = Console.ReadLine();
             try
             {
                 GetTypes(path);
+                OutputSortedMemberTypes();
             }
             catch (InvalidOperationException ex)
             {
