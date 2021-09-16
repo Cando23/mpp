f8d922889f751dcbb0d9d6f7cb0c91ef50b24be3
Roman Polishchuk <rmn.polishchuk@gmail.com>
2021-09-17 00:17:32 +0300

    add TaskQueue constuctor implementation

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -5,7 +5,8 @@ namespace Lab1
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
+            TaskQueue taskQueue = new TaskQueue(2);
             Console.ReadLine();
         }
     }
--- Lab1/TaskQueue.cs
+++ Lab1/TaskQueue.cs
@@ -1,9 +1,34 @@
-﻿using System.Threading;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace Lab1
 {
     public class TaskQueue
     {
+        public delegate void TaskDelegate();
+
+        private readonly Thread[] _threads;
         
+        public TaskQueue(int count)
+        {
+            _threads = new Thread[count];
+            for(var i = 0; i < count; i++)
+            {
+                _threads[i] = new Thread(task) {Name = "Name " + i};
+                _threads[i].Start();
+            }
+        }
+
+        private void task()
+        {
+            for (var i = 0; i < 2; i++)
+            {
+                var a = Thread.CurrentThread.Name;
+                Console.WriteLine(a);
+                Thread.Sleep(200);
+            }
+        }
+
     }
 }
