8c7b64d64cfdc4f53301c802293c079c93a0de0c
Roman Polishchuk <rmn.polishchuk@gmail.com>
2021-09-17 14:02:54 +0300

    add Mutex(Lock and Unlock) methods implementation

---
+++ Lab3/Mutex.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+
+namespace Lab3
+{
+    public class Mutex
+    {
+        /* int InterLocked.CompareExchange(ref int location1, int value, int comparand)
+         * Метод принимает три значения: первое передается по ссылке и это то значение,
+         * которое будет изменено на второе,
+         * если в момент сравнения location1 совпадает с comparand.
+         * то оригинальное значение location1 - результат функции.
+         */
+        private int _locationId = -1;
+        public void Lock()
+        {
+            var id = Thread.CurrentThread.ManagedThreadId;
+            while (Interlocked.CompareExchange(ref _locationId, id, -1) != -1)
+            {
+                Thread.Sleep(100);
+            }
+        }
+
+        public void Unlock()
+        {
+            var id = Thread.CurrentThread.ManagedThreadId;
+            Interlocked.CompareExchange(ref _locationId, -1, id);
+        }
+    }
+}
