72849006dc46ac752712bbcb7e4dc25bb2125aee
Roman Polishchuk <rmn.polishchuk@gmail.com>
2021-09-17 16:15:05 +0300

    add test methods

--- Lab5/Program.cs
+++ Lab5/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace Lab5
+﻿using System;
+using System.Threading;
+namespace Lab5
 {
    static class Program
     {
         static void Main(string[] args)
         {
-
+            TaskQueue.TaskDelegate[] array =  {Display, Factorial, Cycle};
+            Parallel.WaitAll(array);
+        }
+        static void Display()
+        {
+            Console.WriteLine($"Выполняется поток {Thread.CurrentThread.ManagedThreadId}");
+        }
+        static void Cycle()
+        {
+            int result = 1;
+ 
+            for (int i = 1; i <= 10000; i++)
+            {
+                result += i;
+            }
+            Console.WriteLine($"Выполняется поток {Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine($"Результат {result} потока {Thread.CurrentThread.ManagedThreadId}");
+     
+        }
+        static void Factorial()
+        {
+            int result = 1;
+ 
+            for (int i = 1; i <= 10; i++)
+            {
+                result *= i;
+            }
+            Console.WriteLine($"Выполняется поток {Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine($"Результат {result} потока {Thread.CurrentThread.ManagedThreadId}");
         }
     }
 }
