e3da996669598ac1aeff6b1e333ed044192362c0
Roman Polishchuk <rmn.polishchuk@gmail.com>
2021-09-18 18:42:25 +0300

    add Lab2 project files

---
+++ Lab2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                return;
+            }
+        }
+    }
+}
---
+++ Lab2/TaskQueue.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace Lab2
+{
+    public class TaskQueue
+    {
+        public delegate void TaskDelegate();
+
+        private readonly Thread[] _threads;
+
+        private readonly BlockingCollection<TaskDelegate> _taskQueue;
+
+        public bool IsCompleted()
+        {
+            foreach (var thread in _threads)
+            {
+                if (thread.ThreadState == ThreadState.Running)
+                    return false;
+            }
+            return true;
+        }
+        public TaskQueue(int count)
+        {
+            _taskQueue = new BlockingCollection<TaskDelegate>();
+            _threads = new Thread[count];
+            for(var i = 0; i < count; i++)
+            {
+                _threads[i] = new Thread(ProcessQueue);
+                _threads[i].Start();
+            }
+        }
+
+        private void ProcessQueue()
+        {
+            while (true)
+            {
+                var task = _taskQueue.Take();
+                try
+                {
+                    task?.Invoke();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+        
+        public void EnqueueTask(TaskDelegate task)
+        { 
+            _taskQueue.Add(task);
+        }
+    }
+}
